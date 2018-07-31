ac11d631d3c44137b009dc3602fbe567ff3fa904
mark0725 <gaoxilin@gmail.com>
2018-07-31 15:32:57 +0800

    penta-sdk-js v0.1

--- gen.go
+++ gen.go
@@ -1,5 +1,5 @@
 // Package sdk4js contains the console JavaScript dependencies Go embedded.
 package sdk4js
 
-//go:generate go-bindata -nometadata -pkg sdk4js_bin -o ./sdk4js_bin/bindata.go bignumber.js penta.js
+//go:generate go-bindata -nometadata -pkg sdk4js_bin -o ./sdk4js_bin/bindata.go penta.js
 //go:generate gofmt -w -s ./sdk4js_bin/bindata.go
